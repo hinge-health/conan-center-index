ccd866cc3a108343e0d4c5f432b669edb7f92ded
Connor S. MacDonald <connor.macdonald@hingehealth.com>
2022-12-02 15:50:36 -0500

    Fix bitcode not being enabled on iOS

--- recipes/ffmpeg/all/conanfile.py
+++ recipes/ffmpeg/all/conanfile.py
@@ -571,6 +571,9 @@ class FFMpegConan(ConanFile):
                 extra_ldflags.extend(
                     ["-arch {}".format(apple_arch), "-isysroot {}".format(xcrun.sdk_path)])
 
+        if self.settings.os == "iOS" and self.conf.get("tools.apple:enable_bitcode", check_type=bool):
+            extra_cflags.append("-fembed-bitcode")
+
         args.append("--extra-cflags={}".format(" ".join(extra_cflags)))
         args.append("--extra-ldflags={}".format(" ".join(extra_ldflags)))
 
