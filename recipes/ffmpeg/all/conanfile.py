87b1fbde64847d466f3ec11cf98bfacca30a7e54
Connor S. MacDonald <connor.macdonald@hingehealth.com>
2022-12-05 12:18:09 -0500

    Fix wrong cross-prefix when building on Linux

--- recipes/ffmpeg/all/conanfile.py
+++ recipes/ffmpeg/all/conanfile.py
@@ -571,6 +571,11 @@ class FFMpegConan(ConanFile):
                 extra_ldflags.extend(
                     ["-arch {}".format(apple_arch), "-isysroot {}".format(xcrun.sdk_path)])
 
+            if self.settings.os == "Android" and self.settings_build.os == "Linux":
+                cross_prefix = os.path.join(
+                    tools.get_env(["NDK_ROOT"]), "bin", "llvm")
+                args.append(f"--cross-prefix={cross_prefix}")
+
         args.append("--extra-cflags={}".format(" ".join(extra_cflags)))
         args.append("--extra-ldflags={}".format(" ".join(extra_ldflags)))
 
